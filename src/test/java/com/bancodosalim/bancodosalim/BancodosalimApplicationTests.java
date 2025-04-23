73cb4d9f8da3a4085d02d59368d7e83f66b6ec9d
André Luiz <andre.sousa.dev@icloud.com>
2025-04-23 13:05:44 -0300

    Add files via upload

---
+++ src/test/java/com/bancodosalim/bancodosalim/BancodosalimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancodosalim.bancodosalim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodosalimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
